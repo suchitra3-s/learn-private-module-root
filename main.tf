c667d013b8737bb202ea99b0bcbfa7e9b7f53ad8
suchitra3-s <129984126+suchitra3-s@users.noreply.github.com>
2024-12-05 13:10:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/tf_demo_ss/terraform/s3-webapp/aws"
+  source  = "app.terraform.io/tf_demo_ss/s3-webapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
